6ab39a710fad5aa8f3b6379fcdd4eb2de5b6ef89
Livia Costa da Silva <silva.livia@unifesp.br>
2026-05-11 14:42:05 -0300

    Implementação criação de reservas

--- src/GerenciadorReservas.java
+++ src/GerenciadorReservas.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+
 public class GerenciadorReservas {
 
     private Repositorio repositorio;
@@ -14,5 +16,17 @@ public class GerenciadorReservas {
 
     public void adicionarSala(Sala sala) {
         repositorio.getSalas().add(sala);
+    }
+
+    public void criarReserva(Usuario usuario, Sala sala, LocalDateTime inicio, LocalDateTime fim) {
+
+        Reserva novaReserva = new Reserva(usuario, sala, inicio, fim);
+
+        if (politica.podeReservar(novaReserva, repositorio.getReservas())) {
+            repositorio.getReservas().add(novaReserva);
+            usuario.atualizar("Reserva criada com sucesso.");
+        } else {
+            usuario.atualizar("Não foi possível criar a reserva. Existe conflito de horário.");
+        }
     }
 }
