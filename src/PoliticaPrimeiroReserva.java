14150281f7e44cf74805e6bf25cf50b328cdcf45
Livia Costa da Silva <silva.livia@unifesp.br>
2026-05-11 14:46:36 -0300

    Implementação para detecção de colisões de horário

--- src/PoliticaPrimeiroReserva.java
+++ src/PoliticaPrimeiroReserva.java
@@ -1,15 +1,20 @@
 import java.util.List;
 
 public class PoliticaPrimeiroReserva implements PoliticaDeReserva {
+
     @Override
     public boolean podeReservar(Reserva novaReserva, List<Reserva> reservasExistentes) {
+
         for (Reserva reserva : reservasExistentes) {
+
             if (reserva.isConfirmada()
                     && reserva.getSala().getCodigo().equals(novaReserva.getSala().getCodigo())
                     && horariosColidem(novaReserva, reserva)) {
+
                 return false;
             }
         }
+
         return true;
     }
 
